33a05f056604e4a04dfb3d680cb0725792ad452e
zhangrui <code.zhangrui@gmail.com>
2017-04-11 13:42:05 +0800

    基本骨架完成

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import Welcome from './Welcome'
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-ReactDOM.render(
-  <p>啊，美丽的大自然！</p>,
+function tick() {
+
+  /*const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );//element*/
+
+ ReactDOM.render(
+  <Welcome name="zhangrui"/>,
   document.getElementById('root')
-);
+ );
+}
+setInterval(tick,1000);
+
