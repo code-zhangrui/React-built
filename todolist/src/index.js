68b852ad924682018f83b6e8b148ace32a65669c
zhangrui <code.zhangrui@gmail.com>
2017-04-19 18:16:38 +0800

    todolist

---
+++ todolist/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+
+
+  /*const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );//element*/
+
+ ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+ )
+
