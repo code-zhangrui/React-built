bd4441c0d765f37488a0306d15037cdf61e4bbb6
code-zhangrui <code.zhangrui@gmail.com>
2017-06-07 08:34:49 +0800

    不输入内容回车没用

--- hello-world/src/TodoInput.js
+++ hello-world/src/TodoInput.js
@@ -1,20 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './TodoInput.css'
 
 function submit (props, e) {
-   if (e.key === 'Enter') {
-     props.onSubmit(e)
-    }
+	 if (e.key === 'Enter') {
+				 if (e.target.value.trim() !== '') {
+			 props.onSubmit(e)
+		 }
+		}
 }
 function changeTitle (props, e) {
-   props.onChange(e)
+	 props.onChange(e)
  }
- 
- 
+
+
  export default function (props) {
-   return <input type="text" value={props.content}
-     className="TodoInput"
-     onChange={changeTitle.bind(null, props)}
-     onKeyPress={submit.bind(null, props)}/>
+	 return <input type="text" value={props.content}
+		 className="TodoInput"
+		 onChange={changeTitle.bind(null, props)}
+		 onKeyPress={submit.bind(null, props)}/>
  }
- 
