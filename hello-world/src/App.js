498513fe456593bb2dbda22cbecbec92b3d87874
code-zhangrui <code.zhangrui@gmail.com>
2017-06-07 08:36:58 +0800

    删除冗余代码

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,112 +6,96 @@
  import TodoItem from './TodoItem'
  import UserDialog from './UserDialog'
  import {getCurrentUser, signOut} from './leanCloud'
-  
- import AV from './leanCloud'
- 
- // 声明类型
- var TodoFolder = AV.Object.extend('TodoFolder');
- // 新建对象
- var todoFolder = new TodoFolder();
- // 设置名称
- todoFolder.set('name','工作');
- // 设置优先级
- todoFolder.set('priority',1);
- todoFolder.save().then(function (todo) {
-   console.log('objectId is ' + todo.id);
- }, function (error) {
-   console.error(error);
- });
- 
-  class App extends Component {
-    constructor(props){
-      super(props)
-     this.state = {
-       user: getCurrentUser() || {},
-       newTodo: '',
-       todoList: []
-     }
-   }
-   render() {
- 
-     let todos = this.state.todoList
-       .filter((item)=> !item.deleted)
-       .map((item,index)=>{
-       return ( // 为什么这里要加个括号？这是动手题3 🐸
-         <li key={index} >
-           <TodoItem todo={item} onToggle={this.toggle.bind(this)} 
-             onDelete={this.delete.bind(this)}/>
-         </li>
-       )
-     })
- 
-     return (
-       <div className="App">
-         <h1>{this.state.user.username||'我'}的待办
-           {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
-         </h1>
-         <div className="inputWrapper">
-           <TodoInput content={this.state.newTodo} 
-             onChange={this.changeTitle.bind(this)}
-             onSubmit={this.addTodo.bind(this)} />
-         </div>
-         <ol className="todoList">
-           {todos}
-         </ol>
-         {this.state.user.id ? 
-           null : 
-           <UserDialog 
-             onSignUp={this.onSignUpOrSignIn.bind(this)} 
-             onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
-       </div>
-     )
-   }
-   signOut(){
-     signOut()
-     let stateCopy = JSON.parse(JSON.stringify(this.state))
-     stateCopy.user = {}
-     this.setState(stateCopy)
-   }
-   onSignUpOrSignIn(user){
-     let stateCopy = JSON.parse(JSON.stringify(this.state)) 
-     stateCopy.user = user
-     this.setState(stateCopy)
-   }
-   componentDidUpdate(){
-   }
-   toggle(e, todo){
-     todo.status = todo.status === 'completed' ? '' : 'completed'
-     this.setState(this.state) 
-   }
-   changeTitle(event){
-     this.setState({
-       newTodo: event.target.value,
-       todoList: this.state.todoList
-     })
-   }
-   addTodo(event){
-     this.state.todoList.push({
-       id: idMaker(),
-       title: event.target.value,
-       status: null,
-       deleted: false
-     })
-     this.setState({
-       newTodo: '',
-       todoList: this.state.todoList
-     })
-   }
-   delete(event, todo){
-     todo.deleted = true
-     this.setState(this.state) 
-   }
+
+	class App extends Component {
+		constructor(props){
+			super(props)
+		 this.state = {
+			 user: getCurrentUser() || {},
+			 newTodo: '',
+			 todoList: []
+		 }
+	 }
+	 render() {
+
+		 let todos = this.state.todoList
+			 .filter((item)=> !item.deleted)
+			 .map((item,index)=>{
+			 return ( // 为什么这里要加个括号？这是动手题3 🐸
+				 <li key={index} >
+					 <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+						 onDelete={this.delete.bind(this)}/>
+				 </li>
+			 )
+		 })
+
+		 return (
+			 <div className="App">
+				 <h1>{this.state.user.username||'我'}的待办
+					 {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
+				 </h1>
+				 <div className="inputWrapper">
+					 <TodoInput content={this.state.newTodo}
+						 onChange={this.changeTitle.bind(this)}
+						 onSubmit={this.addTodo.bind(this)} />
+				 </div>
+				 <ol className="todoList">
+					 {todos}
+				 </ol>
+				 {this.state.user.id ?
+					 null :
+					 <UserDialog
+						 onSignUp={this.onSignUpOrSignIn.bind(this)}
+						 onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
+			 </div>
+		 )
+	 }
+	 signOut(){
+		 signOut()
+		 let stateCopy = JSON.parse(JSON.stringify(this.state))
+		 stateCopy.user = {}
+		 this.setState(stateCopy)
+	 }
+	 onSignUpOrSignIn(user){
+		 let stateCopy = JSON.parse(JSON.stringify(this.state))
+		 stateCopy.user = user
+		 this.setState(stateCopy)
+	 }
+	 componentDidUpdate(){
+	 }
+	 toggle(e, todo){
+		 todo.status = todo.status === 'completed' ? '' : 'completed'
+		 this.setState(this.state)
+	 }
+	 changeTitle(event){
+		 this.setState({
+			 newTodo: event.target.value,
+			 todoList: this.state.todoList
+		 })
+	 }
+	 addTodo(event){
+		 this.state.todoList.push({
+			 id: idMaker(),
+			 title: event.target.value,
+			 status: null,
+			 deleted: false
+		 })
+		 this.setState({
+			 newTodo: '',
+			 todoList: this.state.todoList
+		 })
+	 }
+	 delete(event, todo){
+		 todo.deleted = true
+		 this.setState(this.state)
+	 }
  }
- 
+
  export default App;
- 
+
  let id = 0
- 
+
  function idMaker(){
-   id += 1
-   return id
+	 id += 1
+	 return id
  }
